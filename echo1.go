df2d37b90c00b071efd9b4cf411566ced36dcaa7
rahulkumarshahi <rahul.mit@gmail.com>
2016-06-19 13:28:28 -0400

    Added echo equivalent

---
+++ echo1.go
@@ -0,0 +1,17 @@
+// Echo1 prints its command-line arguments.
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s, sep string
+	sep = " "
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+	}
+	fmt.Println(s)
+}
